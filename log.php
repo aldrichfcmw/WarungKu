89322f52fa1a470b71af1c6c5f003ce9de8b04ea
aldrichfcmw <aldrichmuktiwibiwo@gmail.com>
2022-04-03 02:53:17 +0700

    dashboard

--- log.php
+++ log.php
@@ -59,7 +59,7 @@ if (isset($_POST['login'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>WarungKu</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-        <link rel="stylesheet" href="asset/log.css">
+        <link rel="stylesheet" href="asset/css/log.css">
     </head>
     <body>
         <input type='checkbox' id='form-switch'>
