f7259aef3f0c6b66e7df6faefec7385b637b15e8
aldrichfcmw <aldrichmuktiwibiwo@gmail.com>
2022-04-07 21:57:20 +0700

    database new

--- warungku.sql
+++ warungku.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2022 at 09:35 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Waktu pembuatan: 07 Apr 2022 pada 16.54
+-- Versi server: 10.4.16-MariaDB
+-- Versi PHP: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,41 +24,112 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `dagangan`
+--
+
+CREATE TABLE `dagangan` (
+  `id_barang` int(255) NOT NULL,
+  `id_kategori` int(255) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `gambar_barang` varchar(255) NOT NULL,
+  `jumlah_barang` int(255) NOT NULL,
+  `harga_barang` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `dagangan`
+--
+
+INSERT INTO `dagangan` (`id_barang`, `id_kategori`, `nama_barang`, `gambar_barang`, `jumlah_barang`, `harga_barang`) VALUES
+(121331, 121, 'Discord Nitro', '', 50, 35000),
+(122312, 122, 'Playstation Network Card', '', 25, 50000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `icon` varchar(255) NOT NULL,
+  `kategori` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `icon`, `kategori`) VALUES
+(121, 'apps.svg', 'Aplikasi'),
+(122, 'console.svg', 'Console Game');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
   `id` int(10) NOT NULL,
+  `level` int(50) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `foto` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(1, 'Admin', 'admin@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
-(2, 'Aldrich FCMW', 'aldrichmuktiwibiwo@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055');
+INSERT INTO `users` (`id`, `level`, `name`, `email`, `password`, `foto`) VALUES
+(1, 0, 'Admin', 'admin@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', ''),
+(2, 1, 'Aldrich FCMW', 'aldrichmuktiwibiwo@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `dagangan`
+--
+ALTER TABLE `dagangan`
+  ADD PRIMARY KEY (`id_barang`),
+  ADD KEY `nama_barang` (`nama_barang`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`),
+  ADD KEY `kategori` (`kategori`);
+
+--
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD KEY `name` (`name`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `dagangan`
+--
+ALTER TABLE `dagangan`
+  MODIFY `id_barang` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122313;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
