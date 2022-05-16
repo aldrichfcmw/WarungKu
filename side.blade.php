1ad72ac2c593caa27dc29fad01035bab92d3a221
aldrichfcmw <aldrichmuktiwibiwo@gmail.com>
2022-05-16 19:11:20 +0700

    dashboard

--- side.blade.php
+++ side.blade.php
@@ -4,7 +4,7 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
         <div class="sidebar-brand-icon rotate-n-15">
-            {{-- <i class="fas fa-laugh-wink"></i> --}}
+            
             <img src="asset/images/icon/Icon WK.png" width="25px" alt="" srcset="">
         </div>
         <div class="sidebar-brand-text mx-3">WK Admin</div>
