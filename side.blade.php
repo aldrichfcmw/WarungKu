654f40e0112b811a2f2400b4096afa809f300cd4
aldrichfcmw <aldrichmuktiwibiwo@gmail.com>
2022-06-16 10:47:52 +0700

    size fixed

--- side.blade.php
+++ side.blade.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index">
         <div class="sidebar-brand-icon rotate-n-15">
             
             <img src="asset/images/icon/Icon WK.png" width="25px" alt="" srcset="">
